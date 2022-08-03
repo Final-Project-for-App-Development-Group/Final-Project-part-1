83f47a7320bef1a549dcdd1cc425cf5e0467bca9
SeanAnderson2022 <108705392+SeanAnderson2022@users.noreply.github.com>
2022-08-03 23:10:20 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devdactic.devpush',
+  appName: 'pushApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
